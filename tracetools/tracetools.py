f2fa535d18d97aac5a973c26a3ca6817df64abe5
Kanelchefen <elias.rhoden@hotmail.com>
2024-05-15 08:57:50 +0200

    Update tracetools.py

--- tracetools/tracetools.py
+++ tracetools/tracetools.py
@@ -4,59 +4,49 @@ from . import tracetypes as tp
 import numpy as np
 import pandas as pd
 
-
 def parse_trace_file(filename)->list[tp.Trace]:
+
     root = ET.parse(filename).getroot()
+    signal_meta_data = _find_signal_names(root)
 
     trace = root.find('traceData')
-
     df = trace.find('dataFrame')
 
-    signal_names = []
-    signal_paths = []
     signals = []
     time_vecs = []
 
-    for c in df:
-        tag = c.tag 
-        if tag == 'dataSignal':
-            signal_names.append(c.attrib['description'])
-            signal_paths.append(c.attrib['name'])
-        if tag == 'rec':
-            break
+    signals = [[] for i in signal_meta_data]
+    time_vecs = [[] for i in signal_meta_data]
 
-    signal_names = [name.replace('(64 bit)','') for name in signal_names]
-
-    signals = [[] for i in signal_names]
-    time_vecs = [[] for i in signal_names]
-
-    for c in df:
-        tag = c.tag 
-        if tag == 'rec':
-            time = float(c.attrib['time'])
-            for att in c.attrib.keys():
-                if att[0] == 'f':
-                    indx = int(att.replace('f','')) - 1
-                    sig_val = float(c.attrib[att])
-                    if sig_val > 9218868437227405000:
-                        if len(signals[indx]) == 0:
-                            sig_val = 0
-                        else:
-                            sig_val = signals[indx][-1]
-                    signals[indx].append(sig_val)
-                    time_vecs[indx].append(time)
-
-
-    # remove first sample
-    signals = [s[1:] for s in signals]
-    time_vecs = [t[1:] for t in time_vecs]
+    for c in df.iter('rec'):
+        time = float(c.attrib['time'])
+        for att in c.attrib.keys():
+            if att[0] == 'f':
+                indx = int(att.replace('f','')) - 1
+                sig_val = float(c.attrib[att])
+                signals[indx].append(sig_val)
+                time_vecs[indx].append(time) 
 
     traces = []
 
-    for i,name in enumerate(signal_names):
-        trace = tp.Trace(name,signal_paths[i],np.array(time_vecs[i]),np.array(signals[i]))
+    for i,data in enumerate(signal_meta_data):
+        name = data[0]
+        descr = data[1]
+        trace = tp.Trace(name,descr,np.array(time_vecs[i]),np.array(signals[i]))
         traces.append(trace)
     return traces
+
+def _find_signal_names(root):
+    dispSetup = root.find('traceDisplaySetup')
+    
+    signals = []
+
+    for s in dispSetup.find("signals"):
+        desc = s.attrib['description']
+        name = s.attrib['name']
+        signals.append((desc,name))
+
+    return signals
 
 def plot_trace(T:tp.Trace,c=''):
     plt.plot(T.time,T.signal,c)
@@ -73,8 +63,7 @@ def save_signals_as_csv(signals:list[tp.Trace],filename):
         data.append(s.signal)
         columns.append(f"time_{signal_name}")
         columns.append(signal_name)
-            
-    
+
     df = pd.DataFrame(data,columns)
 
     df.to_csv(filename.replace('.xml','.csv'))
