0195a23a4943d1e90379b9df1924fccdb2e11619
Kanelchefen <elias.rhoden@hotmail.com>
2024-12-04 15:11:32 +0100

    Included sinu_tf in __init__

--- tracetools/__init__.py
+++ tracetools/__init__.py
@@ -2,4 +2,4 @@
 from .tracetypes import *
 from .tracetools import parse_trace_file, plot_trace, save_signals_as_csv
 from .tuning_files import plot_freq,parse_autotune_file,parse_freq_meas_file,parse_step_response
-
+from .sinu_tf import *
